7d14ef669135ba9a22f818ec67adfb6ad7c95723
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-07-08 23:51:26 +0530

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+import userModel from "../models/userModel.js";
+
+// Will code this later
+
+const loginUser = async function(req, res){};
+
+const registerUser = async function(req,res){};
+
+const adminLogin = async function(req,res){};
+
+export {loginUser, registerUser, adminLogin};
