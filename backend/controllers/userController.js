40ba59fbb1aab92dd25eb673d03af7b2b511b7d3
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-07-17 23:29:17 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,91 @@
 import userModel from "../models/userModel.js";
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-// Will code this later
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
-const loginUser = async function(req, res){};
+const loginUser = async function(req, res){
+    try {
+        
+        const {email, password} = req.body;
 
-const registerUser = async function(req,res){};
+        const userExists = await userModel.findOne({email});
+        if(!userExists){
+            return res.json({success: false, message: "User Doesn't Exists"})
+        }
 
-const adminLogin = async function(req,res){};
+        const isMatch = await bcrypt.compare(password, userExists.password);
 
-export {loginUser, registerUser, adminLogin};
+        if (isMatch) {
+            const token = createToken(userExists._id);
+            res.json({success:true,token})
+        } else{
+            res.json({success:false, message: "Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
+}
+
+const registerUser = async function(req,res){
+    try {
+        const {name, email, password} = req.body;
+
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({success: false, message: 'User Already Exists'})
+        }
+
+        if(!validator.isEmail(email)) {
+            return res.json({success: false, message: 'Please enter a valid email'})
+        }
+
+        if(password.length < 8){
+            return res.json({success:false, message:"Please enter a strong password"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+
+        res.json({success:true, token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
+}
+
+const adminLogin = async function(req,res){
+    try {
+        
+        const {email, password} = req.body;
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success:true,token});
+        } else {
+            res.json({success:false,message:'Invalid Credentials'})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
+}
+
+export {loginUser, registerUser, adminLogin}
